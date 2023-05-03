894b91f1b2e791444558c4402ad0352fbc1d497d
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-05-03 19:52:40 +0100

    Added dictionary methods

---
+++ dictionaries/dictionaries_exercise_8.py
@@ -0,0 +1,20 @@
+'''
+extract from list into tuple
+'''
+
+def extract_from_list_to_tuple(input_list: list) -> list:
+    tuple_list = []
+    idx = 0
+    for input  in input_list:
+        tuple_list.append((idx, input))
+        idx += 1
+
+    return tuple_list    
+
+tickers = [
+    'AAPL.US', 'AXP.US', 'BA.US', 'CAT.US',
+    'CSCO.US', 'CVX.US', 'DIS.US', 'DOW.US',
+    'GS.US', 'HD.US', 'IBM.US', 'INTC.US'
+]
+
+print(extract_from_list_to_tuple(tickers))
