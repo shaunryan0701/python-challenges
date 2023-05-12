d83794e3095512c1a9680ff4acdd94d2d3cbb977
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-05-12 22:37:43 +0100

    Added for loop methods

--- for_loop/for_exercise_8.py
+++ for_loop/for_exercise_8.py
@@ -1,3 +1,19 @@
 '''
 iterate through dictionary for values meeting criteria
 '''
+
+def find_values_in_dict(input_dict: dict, search_value: float) -> list:
+    key_list = [key for key, value in input_dict.items() if value > search_value]
+
+    return key_list
+
+gaming = {
+    '11B': 362.5,
+    'CDR': 297.0,
+    'CIG': 0.85,
+    'PLW': 318.0,
+    'TEN': 300.0
+}
+
+print(find_values_in_dict(gaming, 100.0))
+        
