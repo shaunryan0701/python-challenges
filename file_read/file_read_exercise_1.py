fe1e550a4b3eec36e304b0b360db4dd7523837f3
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-05-21 20:36:58 +0100

    Added file_read methods

--- file_read/file_read_exercise_1.py
+++ file_read/file_read_exercise_1.py
@@ -4,6 +4,12 @@ read stock data and calculate average
 def read_file(file_name: str) -> None:
     with open(file_name, 'r') as file:
         lines = file.read().splitlines()
-        print(lines)
+    
+    closing_price = []
+    for idx, line in enumerate(lines):
+        if idx > 0:
+            print(line)
+            closing_price.append(line.split(',')[-2])
+            print(closing_price)
 
 read_file("playway.txt")
