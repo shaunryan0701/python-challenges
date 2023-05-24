6dc5f96feb165e6c8cca99f6aaa26d883b8f727d
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-05-24 21:13:39 +0100

    Added file_read methods

---
+++ file_read/file_read_exercise_3.py
@@ -0,0 +1,17 @@
+'''
+convert list to dictionary
+'''
+
+with open('playway.csv', 'r') as file:
+    content = file.read().splitlines()
+
+
+data = [(line.split(',')[0], line.split(',')[4]) for line in content]
+
+result = {
+    data [0][0]: [data[1:][i][0] for i in range(len(data) -1)],
+    data [0][1]: [float(data[1:][i][1]) for i in range(len(data) -1)]
+}
+
+print(result)
+
