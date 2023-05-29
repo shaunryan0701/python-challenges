7dc40737c07df92fbd262a3c736a1ca0de43ac55
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-05-29 14:43:58 +0100

    Added file_read methods

--- file_read/file_read_exercise_5.py
+++ file_read/file_read_exercise_5.py
@@ -7,4 +7,8 @@ with open('playway.csv', 'r') as file:
 
 volumes = [(line.split(',')[0], line.split(',')[-1]) for line in content][1:]
 volumes = [(volume[0], int(volume[1])) for volume in volumes]
-print(volumes)
+max_volume = max([val[1] for val in volumes])
+max_date = list(filter(lambda val: val[1] == max_volume, volumes))[0][0]
+# print(max_volume)
+print(max_date)
+# print(volumes)
