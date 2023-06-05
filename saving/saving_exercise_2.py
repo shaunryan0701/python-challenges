568260246f557450f5608e1038e8ae31a62e2dc6
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-06-05 21:35:17 +0100

    Added saving methods

--- saving/saving_exercise_2.py
+++ saving/saving_exercise_2.py
@@ -1,8 +1,9 @@
 '''
 save text to jsom
 '''
+import json
 
 stocks = {'PLW': ['Playway', 350], 'BBT': ['Boombit', 22]}
 
 with open('stocks.json', 'w') as file:
-    file.write(str(stocks))
+    json.dump(stocks, file, indent=4)
