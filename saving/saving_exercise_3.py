ad1c8f06ded28d7f3aa69b3824f27e927b5673f4
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-06-06 22:22:22 +0100

    Added saving methods

--- saving/saving_exercise_3.py
+++ saving/saving_exercise_3.py
@@ -4,3 +4,8 @@ save to csv file
 
 headers = ['user_id', 'amount']
 users = [['001', '1400'], ['004', '1300'], ['007', '900']]
+
+with open('users.csv', 'w') as file:
+    file.write(",".join(headers) + "\n")
+    for user in users:
+      file.write(",".join(user) + "\n")
