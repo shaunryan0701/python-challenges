5fdcda72e4c7c51581ea29fe147fa99b0f18862e
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-06-09 11:00:53 +0100

    Added functions methods

--- functions/functions_exercise_2.py
+++ functions/functions_exercise_2.py
@@ -7,3 +7,9 @@ var2 = ('Python')
 var3 = ('Python',)
 var4 = ['Python']
 var5 = {'Python'}
+
+print(isinstance(var1, tuple))
+print(isinstance(var2, tuple))
+print(isinstance(var3, tuple))
+print(isinstance(var4, tuple))
+print(isinstance(var5, tuple))
