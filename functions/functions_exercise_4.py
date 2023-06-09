2c89ea1c86ac40d102f312b3ee6d224f16b4adc4
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-06-09 15:41:19 +0100

    Added functions methods

---
+++ functions/functions_exercise_4.py
@@ -0,0 +1,9 @@
+'''
+use the zip function to create a list 
+of tuples
+'''
+
+ticker = ('TEN', 'PLW', 'CDR')
+full_name = ('Ten Square Games', 'Playway', 'CD Projekt')
+
+print(list(zip(ticker, full_name)))
---
+++ functions/functions_exercise_5.py
@@ -0,0 +1,7 @@
+'''
+verify if items are truthy
+'''
+
+items = (' ', '0', 0.1, True)
+
+print(all(items))
